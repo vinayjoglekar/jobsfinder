8bb3a71ac932c999a5b8d9a8ef79950211245166
vinayjoglekar <vinayj1990@gmail.com>
2020-07-01 15:12:50 +0530

    initial commit

---
+++ app/src/main/java/com/jovinz/jobsfindingapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jovinz.jobsfindingapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jovinz.jobsfindingapp.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
